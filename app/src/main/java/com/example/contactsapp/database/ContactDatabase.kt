1b490369209f8cd63d7f4c09a25709f786d11552
Yurie-Koga <yurie.koga.a@gmail.com>
2021-06-26 22:57:05 -0700

    Updated DB, Domain, Network for picture url

--- app/src/main/java/com/example/contactsapp/database/ContactDatabase.kt
+++ app/src/main/java/com/example/contactsapp/database/ContactDatabase.kt
@@ -5,8 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
-@Database(entities = [Contact::class], version = 1, exportSchema = false)
+/**
+ * version history
+ * [1] : original
+ * [2] : added = "picture_large"
+ */
+@Database(entities = [Contact::class], version = 2, exportSchema = false)
 abstract class ContactDatabase : RoomDatabase() {
 
     abstract val contactDatabaseDao: ContactDatabaseDao
